9bf2ac8204e3719f8594bfa6cee6d6643ee01e41
Genaro Bedenko (bedenkog) <bedenkog@coventry.ac.uk>
2018-11-15 19:55:34 +0000

    Initialised koa endpoints

---
+++ users-api.js
@@ -0,0 +1,85 @@
+#!/usr/bin/env node
+
+'use strict'
+
+const Koa = require('koa')
+const Router = require('koa-router')
+const bodyParser = require('koa-bodyparser')
+
+const app = new Koa()
+app.use(bodyParser())
+const router = new Router()
+
+const status = require('http-status-codes')
+
+const port = 8082
+
+// app.use( async(ctx, next) => {
+// 	ctx.set('Access-Control-Allow-Origin', '*')
+// 	ctx.set('content-type', 'application/json')
+// 	await next()
+// })
+
+router.get('/api/v1.0/users', async ctx => {
+
+    ctx.set('Allow', 'GET, POST')
+    
+	try {
+
+		if(ctx.get('error')) throw new Error(ctx.get('error'))
+		debugger
+		const users = await usersController.getAll()
+		ctx.status = status.OK
+		ctx.body = {status: 'success', message: {result: users}}
+    }     
+    catch(err) {
+
+		ctx.status = status.NOT_FOUND
+		ctx.body = {status: 'error', message: err.message}
+	}
+})
+
+router.get('/api/v1.0/users/:user', async ctx => {
+
+    ctx.set('Allow', 'GET')
+    
+	try {
+
+		if(ctx.get('error')) throw new Error(ctx.get('error'))
+		ctx.status = status.OK
+		ctx.body = {status: 'success', message: {item: 'xxx'}}
+		const user = await usersController.getUser(ctx.params.user)
+		ctx.status = status.OK
+		ctx.body = {status: 'success', message: {user: user}}
+    }     
+    catch(err) {
+
+		ctx.status = status.NOT_FOUND
+		ctx.body = {status: 'error', message: err.message}
+	}
+})
+
+router.post('/api/v1.0/users', async ctx => {
+
+    ctx.set('Allow', 'POST')
+    
+	try {
+        
+        if(ctx.get('error')) throw new Error(ctx.get('error'))
+        
+		const newUser = await usersController.addUser(ctx.request)
+		ctx.status = status.CREATED
+		ctx.body = {status: 'success', message: {user: newUser}}
+    }     
+    catch(err) {
+
+		ctx.status = status.BAD_REQUEST
+		ctx.body = {status: 'error', message: err.message}
+	}
+})
+
+app.use(router.routes())
+app.use(router.allowedMethods())
+const server = app.listen(port, () => console.log(`Server listening on port ${port}`))
+
+module.exports = server
